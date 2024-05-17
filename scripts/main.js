00616564ef9cd0c624cfdd80b82c17904db6f167
Fabricio Menezes <fabricio-menezes@live.com>
2024-05-17 23:26:27 +0100

    ADD catch

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const endpoint = "https://api.github.com/users/FabricioMeneze5";
+    const errorMessage = $('.errorMessage');
     const avatar = $('#avatar');
     const name = $('.profile-name');
     const user = $('#user-name');
@@ -8,7 +8,7 @@ $(document).ready(function() {
     const following = $('#following');
     const profileLink = $('#profile-link');
 
-    fetch(endpoint)
+    fetch("https://api.github.com/users/FabricioMeneze5")
     .then(function(res) {
         return res.json();
     })
@@ -20,5 +20,11 @@ $(document).ready(function() {
         followers.text(json.followers);
         following.text(json.following);
         profileLink.attr("href", json.html_url);
+    })
+    .catch(function(erro) {
+        errorMessage.css("display", "block");
+    })
+    .finally(function() {
+        console.log("fim");
     })
 })
